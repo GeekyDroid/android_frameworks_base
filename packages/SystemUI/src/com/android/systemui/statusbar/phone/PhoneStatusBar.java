645cf90965f256c380318745499fec1c3c5cd746
zodex <mimirower45@onet.eu>
2014-03-21 11:28:59 +0100

    I think this is last fixes for trds

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2837,7 +2837,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         int uiThemeMode = res.getConfiguration().uiThemeMode;
         if (uiThemeMode != mCurrUiThemeMode) {
             mCurrUiThemeMode = uiThemeMode;
-            recreateStatusBar(false);
             return;
         }
 
